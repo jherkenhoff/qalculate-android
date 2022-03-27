7608797c809885b03c8568a7fe140b0924c7bd3b
ByteHamster <info@bytehamster.com>
2022-03-27 12:11:20 +0200

    Cleanup default project
    Remove or simplify default layouts/fragments

--- app/src/main/java/com/mrkenhoff/qalculate/MainActivity.kt
+++ app/src/main/java/com/mrkenhoff/qalculate/MainActivity.kt
@@ -1,22 +1,13 @@
 package com.mrkenhoff.qalculate
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.mrkenhoff.qalculate.databinding.ActivityMainBinding
 import com.mrkenhoff.libqalculate.Calculator
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-
 
     companion object {
         init {
@@ -26,45 +17,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        supportFragmentManager.beginTransaction()
+            .add(R.id.main_fragment, MainFragment())
+            .commit()
 
         var c = Calculator()
-
         var s = c.calculateAndPrint("1 + 1 + pi", 2000)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, s, Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
