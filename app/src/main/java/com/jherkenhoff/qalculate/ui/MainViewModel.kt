9d3343e4c9db78171977db2d538a712110a33f7f
jherkenhoff <jherkenhoff+github@mailbox.org>
2024-09-14 17:31:10 +0200

    Push libqalculate version to 5.2.0-1

--- app/src/main/java/com/jherkenhoff/qalculate/ui/MainViewModel.kt
+++ app/src/main/java/com/jherkenhoff/qalculate/ui/MainViewModel.kt
@@ -1,6 +1,5 @@
 package com.jherkenhoff.qalculate.ui
 
-import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.text.TextRange
@@ -11,13 +10,9 @@ import androidx.compose.ui.text.input.getTextBeforeSelection
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jherkenhoff.libqalculate.Calculator
-import com.jherkenhoff.libqalculate.EvaluationOptions
 import com.jherkenhoff.libqalculate.IntervalDisplay
-import com.jherkenhoff.libqalculate.MathStructure
-import com.jherkenhoff.libqalculate.ParseOptions
 import com.jherkenhoff.libqalculate.PrintOptions
 import com.jherkenhoff.libqalculate.libqalculateConstants.TAG_TYPE_HTML
-import com.jherkenhoff.libqalculate.libqalculateConstants.TAG_TYPE_TERMINAL
 import com.jherkenhoff.qalculate.data.CalculationHistoryRepository
 import com.jherkenhoff.qalculate.data.model.CalculationHistoryItem
 import com.jherkenhoff.qalculate.domain.CalculateUseCase
@@ -127,12 +122,12 @@ class MainViewModel @Inject constructor(
             val parsedPrintOptions = PrintOptions()
             parsedPrintOptions.place_units_separately = false
             parsedPrintOptions.preserve_format = true
-            parsedPrintOptions.use_unicode_signs = true
+            parsedPrintOptions.use_unicode_signs = 1
             parsedString.value = calculator.print(parsedMathStructure, 2000, parsedPrintOptions, true, 1, TAG_TYPE_HTML)
 
             val resultPo = PrintOptions()
             resultPo.interval_display = IntervalDisplay.INTERVAL_DISPLAY_SIGNIFICANT_DIGITS
-            resultPo.use_unicode_signs = true
+            resultPo.use_unicode_signs = 1
             resultString.value = calculator.print(calculatedMathStructure, 2000, resultPo, true, 1, TAG_TYPE_HTML)
         }
     }
