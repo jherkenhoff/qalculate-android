ca71a48bb2f9d84c7d3db9896810487cc346f895
jherkenhoff <jherkenhoff+github@mailbox.org>
2024-09-28 18:53:05 +0200

    Added settings screen

--- app/src/main/java/com/jherkenhoff/qalculate/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/jherkenhoff/qalculate/ui/settings/SettingsScreen.kt
@@ -3,19 +3,25 @@ package com.jherkenhoff.qalculate.ui.settings
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.outlined.Keyboard
+import androidx.compose.material.icons.outlined.TextFields
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.jherkenhoff.qalculate.R
 
 
 @Composable
@@ -45,7 +51,7 @@ fun SettingsScreenContent(
                     IconButton(onClick = openDrawer) {
                         Icon(
                             imageVector = Icons.Filled.Menu,
-                            contentDescription = "Localized description"
+                            contentDescription = stringResource(R.string.open_menu_content_description)
                         )
                     }
 
@@ -60,6 +66,20 @@ fun SettingsScreenContent(
             modifier = Modifier
                 .padding(innerPadding),
         ) {
+            LazyColumn {
+                item {
+                    ListItem(
+                        headlineContent = { Text("Input") },
+                        supportingContent = { Text("Format, Localization, Autocomplete")},
+                        leadingContent = { Icon(Icons.Outlined.Keyboard, contentDescription = null)})
+                }
+                item {
+                    ListItem(
+                        headlineContent = { Text("Output") },
+                        supportingContent = { Text("Format, Localization, Autocomplete")},
+                        leadingContent = { Icon(Icons.Outlined.TextFields, contentDescription = null)})
+                }
+            }
         }
     }
 }
