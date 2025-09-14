deba9413e8b0b398daed7ba639080d8af00ffeb9
jherkenhoff <jherkenhoff+github@mailbox.org>
2025-09-14 13:03:45 +0200

    Added haptics to keys

--- app/src/main/java/com/jherkenhoff/qalculate/ui/calculator/Key.kt
+++ app/src/main/java/com/jherkenhoff/qalculate/ui/calculator/Key.kt
@@ -1,16 +1,18 @@
 package com.jherkenhoff.qalculate.ui.calculator
 
 import android.view.ViewConfiguration
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.core.EaseInOutQuart
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.awaitFirstDown
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.drag
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -39,17 +41,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.input.pointer.changedToUp
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.input.pointer.positionChange
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
 import com.jherkenhoff.qalculate.model.Key
 import com.jherkenhoff.qalculate.model.KeyAction
@@ -69,7 +73,6 @@ fun Key(
     modifier: Modifier = Modifier,
     onKeyAction: (KeyAction) -> Unit = {}
 ) {
-    var showPopup by remember { mutableStateOf(false) }
 
     val containerColor = when (key.role) {
         KeyRole.NUMBER -> MaterialTheme.colorScheme.surfaceBright
@@ -83,126 +86,135 @@ fun Key(
         KeyRole.SYSTEM -> MaterialTheme.colorScheme.onSecondaryContainer
     }
 
-    val haptic = LocalHapticFeedback.current
 
     when (key) {
-        is Key.DefaultKey ->
-            Box(
-                modifier = modifier
-                    .pointerInput(Unit) {
-                        detectTapGestures(
-                            onPress = { offset ->
-                                showPopup = true
+        is Key.DefaultKey -> DefaultKey(
+            key,
+            onKeyAction = onKeyAction,
+            labelColor = labelColor,
+            containerColor = containerColor,
+            modifier = modifier)
+        is Key.SelectorKey -> SelectorKey(key, onKeyAction = onKeyAction, modifier = modifier)
+    }
+}
+
+@Composable
+fun DefaultKey(
+    key: Key.DefaultKey,
+    modifier: Modifier = Modifier,
+    onKeyAction: (KeyAction) -> Unit = {},
+    labelColor: Color = MaterialTheme.colorScheme.onSurface,
+    containerColor: Color = MaterialTheme.colorScheme.surfaceContainer
+) {
+    val haptic = LocalHapticFeedback.current
+    val coroutineScope = rememberCoroutineScope()
+
+    val longPressTimeout = ViewConfiguration.getLongPressTimeout().toLong()
+
+    var showPopup by remember { mutableStateOf(false) }
+    var popupSecondary by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = modifier.pointerInput(Unit) {
+                awaitPointerEventScope {
+                    while (true) {
+                        awaitFirstDown()
+                        showPopup = true
+                        popupSecondary = false
+
+                        var isLongPress = false
+
+                        val longPressDetectionJob = coroutineScope.launch {
+                            if (key.longClickAction != null) {
+                                delay(longPressTimeout)
+                                popupSecondary = true
+                                isLongPress = true
+                                haptic.performHapticFeedback(HapticFeedbackType.LongPress)
+                            }
+                        }
+
+                        val up = waitForUpOrCancellation()
+
+                        showPopup = false
+                        longPressDetectionJob.cancel()
+
+                        if ( up?.changedToUp() == true) {
+                            if (isLongPress) {
+                                key.longClickAction?.let {
+                                    //haptic.performHapticFeedback(HapticFeedbackType.LongPress)
+                                    onKeyAction(it)
+                                }
+                            } else {
                                 haptic.performHapticFeedback(HapticFeedbackType.VirtualKey)
-                                try {
-                                    awaitRelease()
-                                    key.clickAction?.let { onKeyAction(it) }
-                                } finally {
-                                    delay(50)
-                                    showPopup = false
-                                }
-                            },
-                            onLongPress = {
-                                key.longClickAction?.let { onKeyAction(it) }
-                            }
-                        )
-                    }
-            ) {
-
-                val popupEnabled = when(key.role) {
-                    KeyRole.SYSTEM -> false
-                    else -> true
-                }
-
-                if (showPopup && popupEnabled) {
-                    Layout(
-                        content = {
-                            Surface(
-                                color = MaterialTheme.colorScheme.surfaceVariant,
-                                shape = CircleShape,
-                                shadowElevation = 3.dp,
-                                modifier = Modifier.size(38.dp),
-                            ) {
-                                Box(
-                                    modifier = Modifier.fillMaxSize(),
-                                    contentAlignment = Alignment.Center
-                                ) {
-                                    when (val label = key.clickAction.label) {
-                                        is KeyLabel.Blank -> null
-
-                                        is KeyLabel.Text -> Text(
-                                            label.text,
-                                            color = labelColor,
-                                            style = MaterialTheme.typography.labelLarge
-                                        )
-
-                                        is KeyLabel.Icon -> Icon(
-                                            label.icon,
-                                            label.description,
-                                            tint = labelColor,
-                                            modifier = Modifier.size(MaterialTheme.typography.labelLarge.lineHeight.toDp())
-                                        )
-                                    }
-                                }
+                                onKeyAction(key.clickAction)
                             }
                         }
-                    ) { measurables, constraints ->
-                        val placeable = measurables.first().measure(constraints)
-                        layout(width = 0, height = 0) {
-                            placeable.placeRelative(x = (constraints.maxWidth / 2 - placeable.width / 2), y = -placeable.height - 8.dp.roundToPx())
-                        }
-                    }
-                }
 
-                Surface(
-                    shape = RoundedCornerShape(6.dp),
-                    color = containerColor,
-                    modifier = modifier.fillMaxSize()
-                ) {
-                    Box {
-                        when (val label = key.clickAction.label) {
-                            is KeyLabel.Blank -> null
-
-                            is KeyLabel.Text -> Text(
-                                label.text,
-                                color = labelColor,
-                                style = MaterialTheme.typography.labelLarge,
-                                modifier = Modifier.align(Alignment.Center)
-                            )
-
-                            is KeyLabel.Icon -> Icon(
-                                label.icon,
-                                label.description,
-                                tint = labelColor,
-                                modifier = Modifier.align(Alignment.Center).size(MaterialTheme.typography.labelLarge.lineHeight.toDp())
-                            )
-                        }
-
-                        key.longClickAction?.let {
-                            when (val label = it.label) {
-                                is KeyLabel.Blank -> null
-
-                                is KeyLabel.Text -> Text(
-                                    label.text,
-                                    color = labelColor,
-                                    style = MaterialTheme.typography.labelSmall,
-                                    modifier = Modifier.align(Alignment.TopCenter)
-                                )
-
-                                is KeyLabel.Icon -> Icon(
-                                    label.icon,
-                                    label.description,
-                                    tint = labelColor,
-                                    modifier = Modifier.align(Alignment.TopCenter).size(MaterialTheme.typography.labelSmall.lineHeight.toDp())
-                                )
-                            }
-                        }
                     }
                 }
             }
+    ) {
 
-        is Key.SelectorKey -> SelectorKey(key, onKeyAction = onKeyAction, modifier = modifier)
+        val popupEnabled = when(key.role) {
+            KeyRole.SYSTEM -> false
+            else -> true
+        }
 
+        if (showPopup && popupEnabled) {
+            Layout(
+                content = {
+                    Surface(
+                        color = MaterialTheme.colorScheme.surfaceVariant,
+                        shape = CircleShape,
+                        shadowElevation = 3.dp,
+                        modifier = Modifier.defaultMinSize(32.dp, 32.dp),
+                    ) {
+                        Box(
+                            modifier = Modifier.wrapContentSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            AnimatedContent(popupSecondary) {
+                                val label = if (it && key.longClickAction != null) key.longClickAction.popupLabel else key.clickAction.popupLabel
+                                label?.let {
+                                    KeyLabel(
+                                        label = label,
+                                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                                        modifier = Modifier.padding(horizontal = 8.dp)
+                                    )
+                                }
+                            }
+                        }
+                    }
+                },
+            ) { measurables, constraints ->
+                val placeable = measurables.first().measure(constraints)
+                layout(width = 0, height = 0) {
+                    placeable.placeRelative(x = (constraints.maxWidth / 2 - placeable.width / 2), y = -placeable.height - 8.dp.roundToPx())
+                }
+            }
+        }
+
+        Surface(
+            shape = RoundedCornerShape(6.dp),
+            color = containerColor,
+            modifier = modifier.fillMaxSize().align(Alignment.Center)
+        ) {
+            Box {
+                KeyLabel(
+                    label = key.clickAction.label,
+                    color = labelColor,
+                    modifier = Modifier.align(Alignment.Center)
+                )
+                key.longClickAction?.let {
+                    KeyLabel(
+                        label = it.label,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        style = MaterialTheme.typography.labelSmall,
+                        modifier = Modifier.align(if (it.label != null) Alignment.TopCenter else Alignment.TopEnd)
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -244,6 +256,7 @@ fun SelectorKey(
                         val longPressDetectionJob = coroutineScope.launch {
                             delay(longPressTimeout)
                             isSelecting = true
+                            haptic.performHapticFeedback(HapticFeedbackType.GestureThresholdActivate)
                         }
 
                         drag(down.id) { change ->
@@ -252,6 +265,7 @@ fun SelectorKey(
                             if (!isSelecting && abs(dragAmount) > touchSlop) {
                                 longPressDetectionJob.cancel()
                                 isSelecting = true
+                                haptic.performHapticFeedback(HapticFeedbackType.GestureThresholdActivate)
                             }
 
                             if (isSelecting) {
@@ -263,17 +277,20 @@ fun SelectorKey(
                                 scrollOffset = dragOffsetItemIndex*itemHeightPx + EaseInOutQuart.transform(dragOffsetFractionalIndex)*itemHeightPx
 
                                 val selectedIndex = (nItems/2 - scrollOffset / itemHeightPx).roundToInt().coerceIn(0, key.actions.lastIndex)
-                                selectedAction = key.actions[selectedIndex]
-
-                                //haptic.performHapticFeedback(HapticFeedbackType.SegmentTick)
+                                val newSelectedAction = key.actions[selectedIndex]
+                                if (newSelectedAction != selectedAction) {
+                                    haptic.performHapticFeedback(HapticFeedbackType.SegmentFrequentTick)
+                                }
+                                selectedAction = newSelectedAction
                             }
 
                             change.consume()
                         }
                         longPressDetectionJob.cancel()
 
-                        onKeyAction(selectedAction)
                         isSelecting = false
+                        haptic.performHapticFeedback(HapticFeedbackType.GestureEnd)
+                        onKeyAction(selectedAction)
                     }
                 }
             },
@@ -294,21 +311,10 @@ fun SelectorKey(
                     tint = MaterialTheme.colorScheme.onSurfaceVariant,
                     modifier = Modifier.align(Alignment.TopCenter).size(12.dp)
                 )
-                when (val label = selectedAction.label) {
-                    is KeyLabel.Blank -> null
-
-                    is KeyLabel.Text ->
-                        Text(
-                            text = label.text,
-                            style = MaterialTheme.typography.labelLarge,
-                            modifier = Modifier.align(Alignment.Center)
-                        )
-                    is KeyLabel.Icon -> Icon(
-                        label.icon,
-                        label.description,
-                        modifier = Modifier.align(Alignment.Center).size(MaterialTheme.typography.labelLarge.lineHeight.toDp())
-                    )
-                }
+                KeyLabel(
+                    selectedAction.label,
+                    modifier = Modifier.align(Alignment.Center)
+                )
                 Icon(
                     Icons.Default.KeyboardArrowDown,
                     contentDescription = null,
@@ -339,29 +345,16 @@ fun SelectorKey(
                             .offset(y = scrollOffset.toDp()),
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
-                        for ((index, unit) in key.actions.withIndex()) {
+                        for ((index, action) in key.actions.withIndex()) {
                             val distanceFromCenter = abs(index*itemHeightPx + scrollOffset - centerOffsetPx + itemHeightPx/2)/itemHeightPx
                             val alpha = 1f - (distanceFromCenter * 0.3f)
                             val scale = 1f - (distanceFromCenter * 0.1f)
 
-                            when (val label = unit.label) {
-                                is KeyLabel.Blank -> null
-
-                                is KeyLabel.Text ->
-                                    Text(
-                                        text = label.text,
-                                        fontSize = 18.sp * scale.coerceIn(0.5f, 1f),
-                                        color = Color.White.copy(alpha.coerceIn(0.5f, 1f)),
-                                        modifier = Modifier
-                                            .height(itemHeight)
-                                    )
-                                is KeyLabel.Icon -> Icon(
-                                    label.icon,
-                                    label.description,
-                                    tint = Color.White.copy(alpha.coerceIn(0.5f, 1f)),
-                                    modifier = Modifier.size(MaterialTheme.typography.labelLarge.lineHeight.toDp())
-                                )
-                            }
+                            KeyLabel(
+                                action.label,
+                                color = Color.White.copy(alpha.coerceIn(0.5f, 1f)),
+                                modifier = Modifier.height(itemHeight).scale(scale.coerceIn(0.5f, 1f))
+                            )
                         }
                     }
 
@@ -381,14 +374,48 @@ fun SelectorKey(
     }
 }
 
+@Composable
+fun KeyLabel(
+    label: KeyLabel?,
+    color: Color = MaterialTheme.colorScheme.onSurface,
+    style: TextStyle = MaterialTheme.typography.labelLarge,
+    modifier: Modifier = Modifier
+) {
+    when (label) {
+        null ->
+            Box(
+                modifier
+                    .padding(2.dp)
+                    .size(3.dp)
+                    .background(MaterialTheme.colorScheme.outlineVariant, CircleShape)
+            )
+
+        is KeyLabel.Text ->
+            Text(
+                text = label.text,
+                color = color,
+                style = style,
+                modifier = modifier
+            )
+
+        is KeyLabel.Icon ->
+            Icon(
+                label.icon,
+                label.description,
+                tint = color,
+                modifier = modifier.size(style.fontSize.toDp())
+            )
+    }
+}
+
 
 @Preview
 @Composable
 private fun SingleActionDefaultKeyPreview() {
     Row(Modifier.height(54.dp), horizontalArrangement = Arrangement.spacedBy(6.dp)) {
         Key(Keys.key0, Modifier.weight(1f))
-        Key(Keys.key1, Modifier.weight(1f))
-        Key(Keys.key2, Modifier.weight(1f))
+        Key(Keys.keyBackspace, Modifier.weight(1f))
+        Key(Keys.keyPi, Modifier.weight(1f))
     }
 }
 
