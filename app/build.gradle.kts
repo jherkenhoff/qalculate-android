faa02409a50c52e77b74d8d68502217a6ce3aa63
jherkenhoff <jherkenhoff+github@mailbox.org>
2025-12-22 17:50:36 +0100

    Bump version to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.jherkenhoff.qalculate"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.2.0"
+        versionCode = 5
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {useSupportLibrary = true }
