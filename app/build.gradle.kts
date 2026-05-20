6cbf2adee46d3dc63b9da7339eb9f970d3af3985
jherkenhoff <jherkenhoff+github@mailbox.org>
2026-05-20 08:23:51 +0200

    Update compose BOM version to fix android studio compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
