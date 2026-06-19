0d4311a02e0f275e82d721778528888deeb4215c
jherkenhoff <jherkenhoff+github@mailbox.org>
2026-06-19 08:41:02 +0200

    Remove explicit material3 version since I found that material expressive's ListItem is not what I need

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,7 +99,6 @@ android {
         compose = true
         // Disable unused AGP features
         aidl = false
-        renderScript = false
         shaders = false
         buildConfig = true
     }
@@ -115,7 +114,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.material3)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
